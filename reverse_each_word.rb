d0fa1e47190b3481ad298208b98592681e035f9e
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-27 03:00:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word(phrase)
-  phrase.reverse.split.reverse.join(" ")
+  phrase.reverse = phrase_reverse
+  phrase_reverse.split = split_reversed_phrase
+  slpilt_reversed_phrase.reverse = reverersed_split
+  reverersed_split.join(" ")
 end
   
